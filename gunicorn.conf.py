7d1d6198591bf4265dbc5e16db6cea082acb6318
Diego Anguita <tralcan@gmail.com>
2025-05-15 13:22:45 -0400

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,4 @@
+workers = 1  # Usar solo 1 worker para minimizar memoria
+threads = 1  # Usar solo 1 hilo por worker
+worker_class = "sync"
+timeout = 120  # Aumentar el tiempo de espera para solicitudes lentas
